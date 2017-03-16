2a7e9fd24f821e3b23804f0db14ccd0d45d1b96a
fujinam2 <tfujinami@ymail.com>
2017-03-16 10:56:56 -0700

    getting closer....

--- scripts/supervisor_project.py
+++ scripts/supervisor_project.py
@@ -26,26 +26,28 @@ class Supervisor:
     self.path_goal = rospy.Subscriber('/turtlebot_controller/path_goal', Path, self.path_update)
     rospy.Subscriber('/turtlebot_controller/toSupervisor', String, self.updateFlag)
     self.debug = rospy.Publisher('/turtlebot_controller/supervisor_debug', String, queue_size=10)
+    rospy.Subscriber('/turtlebot_controller/path_validity', Int32MultiArray, self.update_path_validity)
     self.trans_listener = tf.TransformListener()
     self.trans_broad = tf.TransformBroadcaster()
     rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.rviz_goal_callback)    # rviz "2D Nav Goal"
     self.has_tag_location = False
-    self.x_g = 0.0
-    self.y_g = 0.0
-    self.th_g = 0.0
-    self.state = "manual driving"
+    self.state = "normal"
     self.controlFlag = "not done"
-    self.goal_idx = 0   # for tracking which tag number we're going to
+    self.goal_idx = 1   # for tracking which tag number we're going to
     # rospy.Subscriber('/gazebo/model_states', ModelStates, self.gazebo_callback)
     # use goal locations to store the tag locations in the order that we want to visit them for now
-    self.goal_locations = np.array([[2.0, 3.0, 0.0]])#,[3,1,0]])
+    self.goal_locations = np.array([[0.5, 3.0, 0.0]])#,[3,1,0]])
     self.path_locations = self.goal_locations
+    self.x_g = self.goal_locations[0,0]
+    self.y_g = self.goal_locations[0,1]
+    self.th_g = self.goal_locations[0,2]
     self.path_index = 0
     self.path_point = self.goal_locations[0]
     self.end_of_path = False
-    self.last_n = 0
-    self.update_cnt = 0 # used so only run path_update every 10s
+    #self.last_n = 0
+    #self.update_cnt = 0 # used so only run path_update every 10s
     self.man_pose = [0,0,0]
+    self.path_valid = False;
 
     # Fiducial location initialization
     self.waypoint_locations = {}    # dictionary that caches the most updated locations of each mission waypoint
@@ -79,6 +81,10 @@ class Supervisor:
   def updateMission(self,data):
       self.mission = data.data
 
+  def update_path_validity(self, flag):
+      rospy.loginfo('recieved: %s', flag.data)
+      self.path_valid = flag.data
+
   def rviz_goal_callback(self, msg):
       self.man_pose = pose_to_xyth(msg.pose)    # example usage of the function pose_to_xyth (defined above)
       # this callback does nothing... yet!
@@ -104,13 +110,14 @@ class Supervisor:
     self.controlFlag = data.data
 
   def path_update(self, path):
-    if self.update_cnt%50 == 0:
+    if not(self.path_valid):
+      # need a new path, so update
       n = len(path.poses)
       if n == 1:
         # put the one and only point on the path
         x = path.poses[0].pose.position.x
         y = path.poses[0].pose.position.y
-        new_path_locs = np.array([[x,y,0]])
+        self.path_locations = np.array([[x,y,0]])
         self.path_index = 0
       else:
         for i in range(0,n-1):
@@ -128,18 +135,56 @@ class Supervisor:
         y = path.poses[-1].pose.position.y
         th = new_path_locs[-1,2]
         new_path_locs = np.vstack((new_path_locs, [x,y,th]))
-
-      if np.any(new_path_locs[:,0:2] != self.path_locations[self.path_index:,0:2]):
-        # have a new path! takes into accound differing path lens too
         self.path_locations = new_path_locs
-        self.path_index = 1
-        self.path_point = self.path_locations[0,:]
-        if self.path_index == self.path_locations.shape[0]:
-          self.end_of_path = True
-        else:
+        if n>1:
+          self.path_index = 2
+          self.path_point = new_path_locs[1,:]
           self.end_of_path = False
-      # else don't change the current path/path_index
-    self.update_cnt += 1
+        else:
+          self.path_index = 1
+          self.path_point = new_path_locs[0,:]
+          self.end_of_path = True
+
+      
+
+#  def path_update(self, path):
+#    if self.update_cnt%50 == 0:
+#      n = len(path.poses)
+#      if n == 1:
+#        # put the one and only point on the path
+#        x = path.poses[0].pose.position.x
+#        y = path.poses[0].pose.position.y
+#        new_path_locs = np.array([[x,y,0]])
+#        self.path_index = 0
+#      else:
+#        for i in range(0,n-1):
+#          x = path.poses[i].pose.position.x
+#          x2 = path.poses[i + 1].pose.position.x
+#          y = path.poses[i].pose.position.y
+#          y2 = path.poses[i + 1].pose.position.y
+#          th = np.arctan2((y2 - y), (x2 - x))
+#          if i == 0:
+#            new_path_locs = np.array([x,y,th])
+#          else:
+#            new_path_locs = np.vstack((new_path_locs, [x,y,th]))
+#        # add last point:
+#        x = path.poses[-1].pose.position.x
+#        y = path.poses[-1].pose.position.y
+#        th = new_path_locs[-1,2]
+#        new_path_locs = np.vstack((new_path_locs, [x,y,th]))
+#
+#
+#      if np.any(new_path_locs[:,0:2] != self.path_locations[self.path_index:,0:2]):
+#        # have a new path! takes into accound differing path lens too
+#        self.path_locations = new_path_locs
+#        self.path_index = 1
+#        self.path_point = self.path_locations[0,:]
+#        if self.path_index == self.path_locations.shape[0]:
+#          self.end_of_path = True
+#        else:
+#          self.end_of_path = False
+#      # else don't change the current path/path_index
+#    self.update_cnt += 1
   
 #    if n != self.last_n:
 #      self.path_index = 1
@@ -195,7 +240,7 @@ class Supervisor:
       # spin in place for now and look for a tag???
 	    ######################################
       # code to publish location goal here?
-    self.has_tag_location = True    # for 
+    self.has_tag_location = True    # for now
     if self.state == "manual driving":
       self.update_waypoints()
       self.path_point = self.man_pose
@@ -239,23 +284,23 @@ class Supervisor:
 
     # dummy supervisor constant goal:
     
-#    navTo = Float32MultiArray()
-#    navTo.layout.dim.append(MultiArrayDimension())
-#    navTo.layout.dim[0].label = "length"
-#    navTo.layout.dim[0].size = 3
-#    navTo.layout.dim[0].stride = 1
-#    navTo.data = [self.x_g,self.y_g,self.th_g]
-#    self.nav_goal.publish(navTo)
-#    
-#    driveTo = Float32MultiArray()
-#    driveTo.layout.dim.append(MultiArrayDimension())
-#    driveTo.layout.dim[0].label = "length"
-#    driveTo.layout.dim[0].size = 3
-#    driveTo.layout.dim[0].stride = 1
-#    driveTo.data = self.path_point
-#    self.pos_sp_pub.publish(driveTo)
-#    # debug:
-#    self.debug.publish(str(self.goal_idx))
+    navTo = Float32MultiArray()
+    navTo.layout.dim.append(MultiArrayDimension())
+    navTo.layout.dim[0].label = "length"
+    navTo.layout.dim[0].size = 3
+    navTo.layout.dim[0].stride = 1
+    navTo.data = [self.x_g,self.y_g,self.th_g]
+    self.nav_goal.publish(navTo)
+    
+    driveTo = Float32MultiArray()
+    driveTo.layout.dim.append(MultiArrayDimension())
+    driveTo.layout.dim[0].label = "length"
+    driveTo.layout.dim[0].size = 3
+    driveTo.layout.dim[0].stride = 1
+    driveTo.data = self.path_point
+    self.pos_sp_pub.publish(driveTo)
+    # debug:
+    self.debug.publish(str(self.goal_idx))
     # self.debug.publish(str(self.path_point) + str(self.path_index) + " "+str(self.path_locations.shape[0]))
 
     self.controlType.publish(self.state)
