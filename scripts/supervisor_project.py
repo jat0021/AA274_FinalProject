bf10e39333d51c8f583bbe998f0754a4fc2b4953
fujinam2 <tfujinami@ymail.com>
2017-03-15 17:33:25 -0700

    added manual exploration state

--- scripts/supervisor_project.py
+++ scripts/supervisor_project.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 import rospy
 from gazebo_msgs.msg import ModelStates
-from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Float32MultiArray, Int32MultiArray
 from std_msgs.msg import MultiArrayDimension
 from std_msgs.msg import String
 from nav_msgs.msg import Path
 import tf
 import numpy as np
+from geometry_msgs.msg import PoseStamped
+
+def pose_to_xyth(pose):
+  th = tf.transformations.euler_from_quaternion((pose.orientation.x,
+                                                 pose.orientation.y,
+                                                 pose.orientation.z,
+                                                 pose.orientation.w))[2]
+  return [pose.position.x, pose.position.y, th]
 
 class Supervisor:
 
@@ -20,22 +28,41 @@ class Supervisor:
     self.debug = rospy.Publisher('/turtlebot_controller/supervisor_debug', String, queue_size=10)
     self.trans_listener = tf.TransformListener()
     self.trans_broad = tf.TransformBroadcaster()
+    rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.rviz_goal_callback)    # rviz "2D Nav Goal"
     self.has_tag_location = False
     self.x_g = 0.0
     self.y_g = 0.0
     self.th_g = 0.0
-    self.state = "normal"
+    self.state = "manual driving"
     self.controlFlag = "not done"
     self.goal_idx = 0   # for tracking which tag number we're going to
     # rospy.Subscriber('/gazebo/model_states', ModelStates, self.gazebo_callback)
     # use goal locations to store the tag locations in the order that we want to visit them for now
-    self.goal_locations = np.array([[-2.0, 3.0, 0.0]])#np.array([[0,0,-np.pi/4.0],[-2.0, 3.0, 0.0]])
+    self.goal_locations = np.array([[2.0, 3.0, 0.0]])#,[3,1,0]])
     self.path_locations = self.goal_locations
     self.path_index = 0
     self.path_point = self.goal_locations[0]
     self.end_of_path = False
     self.last_n = 0
     self.update_cnt = 0 # used so only run path_update every 10s
+    self.man_pose = [0,0,0]
+
+    # Fiducial location initialization
+    self.waypoint_locations = {}    # dictionary that caches the most updated locations of each mission waypoint
+    self.waypoint_offset = PoseStamped()
+    self.waypoint_offset.pose.position.z = .4    # waypoint is located 40cm in front of the AprilTag, facing it
+    quat = tf.transformations.quaternion_from_euler(0., np.pi/2, np.pi/2)
+    self.waypoint_offset.pose.orientation.x = quat[0]
+    self.waypoint_offset.pose.orientation.y = quat[1]
+    self.waypoint_offset.pose.orientation.z = quat[2]
+    self.waypoint_offset.pose.orientation.w = quat[3]
+
+    # Subscriber to mission goals
+    rospy.Subscriber('/mission', Int32MultiArray, self.updateMission)
+    self.mission = []
+
+    # Debug publisher with tag locations (float32 version)
+    self.debug_pub = rospy.Publisher('/turtlebot_controller/supDebug', Float32MultiArray, queue_size = 10)
 
 #  def gazebo_callback(self, data):
 #    pose = data.pose[data.name.index("mobile_base")]
@@ -49,12 +76,35 @@ class Supervisor:
 #                    "mobile_base",
 #                    "world")
 
+  def updateMission(self,data):
+      self.mission = data.data
+
+  def rviz_goal_callback(self, msg):
+      self.man_pose = pose_to_xyth(msg.pose)    # example usage of the function pose_to_xyth (defined above)
+      # this callback does nothing... yet!
+
+  def update_waypoints(self):
+    for tag_number in self.mission:
+      try:
+        self.waypoint_offset.header.frame_id = "/tag_{0}".format(tag_number)
+        self.waypoint_locations[tag_number] = self.trans_listener.transformPose("/map", self.waypoint_offset)
+
+      except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+        pass
+
+      if tag_number in self.waypoint_locations:
+        # Publish debug message
+        debugMsg = Float32MultiArray()
+        debugMsg.data = np.array([tag_number, self.waypoint_locations[tag_number].pose.position.x, self.waypoint_locations[tag_number].pose.position.y])
+        self.debug_pub.publish(debugMsg)
+
+
   def updateFlag(self, data):
     rospy.loginfo('received: %s', data.data)
     self.controlFlag = data.data
 
   def path_update(self, path):
-    if self.update_cnt%100 == 0:
+    if self.update_cnt%50 == 0:
       n = len(path.poses)
       if n == 1:
         # put the one and only point on the path
@@ -146,8 +196,19 @@ class Supervisor:
 	    ######################################
       # code to publish location goal here?
     self.has_tag_location = True    # for 
-    if self.has_tag_location == False and self.controlFlag == "not done": 
-      self.state = "searching" 
+    if self.state == "manual driving":
+      self.update_waypoints()
+      self.path_point = self.man_pose
+      driveTo = Float32MultiArray()
+      driveTo.layout.dim.append(MultiArrayDimension())
+      driveTo.layout.dim[0].label = "length"
+      driveTo.layout.dim[0].size = 3
+      driveTo.layout.dim[0].stride = 1
+      driveTo.data = self.path_point
+      self.pos_sp_pub.publish(driveTo)
+
+    #elif self.has_tag_location == False and self.controlFlag == "not done": 
+    #  self.state = "searching" 
     elif self.controlFlag == "at dest":
       if self.end_of_path:
         if self.goal_idx == len(self.goal_locations):
@@ -178,23 +239,23 @@ class Supervisor:
 
     # dummy supervisor constant goal:
     
-    navTo = Float32MultiArray()
-    navTo.layout.dim.append(MultiArrayDimension())
-    navTo.layout.dim[0].label = "length"
-    navTo.layout.dim[0].size = 3
-    navTo.layout.dim[0].stride = 1
-    navTo.data = [self.x_g,self.y_g,self.th_g]
-    self.nav_goal.publish(navTo)
-    
-    driveTo = Float32MultiArray()
-    driveTo.layout.dim.append(MultiArrayDimension())
-    driveTo.layout.dim[0].label = "length"
-    driveTo.layout.dim[0].size = 3
-    driveTo.layout.dim[0].stride = 1
-    driveTo.data = self.path_point
-    self.pos_sp_pub.publish(driveTo)
-    # debug:
-    self.debug.publish(str(self.goal_idx))
+#    navTo = Float32MultiArray()
+#    navTo.layout.dim.append(MultiArrayDimension())
+#    navTo.layout.dim[0].label = "length"
+#    navTo.layout.dim[0].size = 3
+#    navTo.layout.dim[0].stride = 1
+#    navTo.data = [self.x_g,self.y_g,self.th_g]
+#    self.nav_goal.publish(navTo)
+#    
+#    driveTo = Float32MultiArray()
+#    driveTo.layout.dim.append(MultiArrayDimension())
+#    driveTo.layout.dim[0].label = "length"
+#    driveTo.layout.dim[0].size = 3
+#    driveTo.layout.dim[0].stride = 1
+#    driveTo.data = self.path_point
+#    self.pos_sp_pub.publish(driveTo)
+#    # debug:
+#    self.debug.publish(str(self.goal_idx))
     # self.debug.publish(str(self.path_point) + str(self.path_index) + " "+str(self.path_locations.shape[0]))
 
     self.controlType.publish(self.state)
